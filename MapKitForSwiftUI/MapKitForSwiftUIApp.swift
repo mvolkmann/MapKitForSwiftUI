d32e74f5807f2b7299cc1a74cdcf20de5dcda5f7
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2023-06-15 09:17:38 -0500

    Initial Commit

---
+++ MapKitForSwiftUI/MapKitForSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapKitForSwiftUIApp.swift
+//  MapKitForSwiftUI
+//
+//  Created by Mark Volkmann on 6/15/23.
+//
+
+import SwiftUI
+
+@main
+struct MapKitForSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
